0fc27240826198beca821ed124b7c02ce0b31c8f
Hatef <letstan009@gmail.com>
2025-09-09 19:06:47 +0330

    V1

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     BOT_TOKEN: str
     MONGO_URI: str
     MONGO_DB: str = "vira-vpn"
-    SUPPORT_USERNAME: str = "vira_support"
+    SUPPORT_USERNAME: str = "stanwitley"
     admin_ids: list[int] = [149609494]
 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
